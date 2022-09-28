de48a59a80fbc628e47cf0def55455749486e051
Garin <pooxiaolin@gmail.com>
2022-09-28 16:24:09 -0700

    Update AutoWarpIp1.sh

--- AutoWarpIp1.sh
+++ AutoWarpIp1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Area="JP"
+Area="DE"
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
 Font_Black="\033[30m";
 Font_Red="\033[31m";
